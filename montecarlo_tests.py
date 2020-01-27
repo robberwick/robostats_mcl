0bd38c4ca8047f756b5485e3700c1b37abeadd0b
markmellors <mark.mellors@gmail.com>
2020-01-27 17:23:11 +0000

    all tests now run ok

--- montecarlo_tests.py
+++ montecarlo_tests.py
@@ -22,50 +22,32 @@ plt.style.use('ggplot')
 import matplotlib.animation as animation
 
 wean_hall_map = mcl.occupancy_map('data/map/wean.dat')
-print("1")
 logdata = mcl.load_log('data/log/robotdata1.log.gz')
-print("2")
-logdata_scans = logdata.query('type > 0.1')
-print("3")
 
 #Initialize 100 particles uniformly in valid locations on the map
 laser = mcl.laser_sensor(stdv_cm=20, uniform_weight=0.2)
-print("3")
-particle_list = [mcl.robot_particle(wean_hall_map, laser, log_prob_descale=100,
+particle_list = [mcl.robot_particle(wean_hall_map, laser, log_prob_descale=50,
                                     sigma_fwd_pct=0.3, sigma_theta_pct=0.2)
-                 for _ in range(300)]
+                 for _ in range(30000)]
+scan_data_gen = (msg for msg in logdata.query('type > 0.1').values)
 
-print("4")
-# Pre-run first couple steps - hard to draw on screen
-#for message in logdata_scans.values[:30:10]:
-#    particle_list = mcl.mcl_update(particle_list, message, 
-#                                    target_particles=300) # Update
+fig, ax = plt.subplots(figsize=(40,40))
+#mcl.draw_map_state(wean_hall_map, particle_list[::20], ax=ax)
+new_particle_list = mcl.mcl_update(particle_list, next(scan_data_gen))
+weights = pd.Series([p.weight for p in new_particle_list])
+new_particle_list = mcl.mcl_update(new_particle_list, next(scan_data_gen))
+weights = pd.Series([p.weight for p in new_particle_list])
+#mcl.draw_map_state(wean_hall_map, new_particle_list, ax=ax)
+new_particle_list = mcl.mcl_update(new_particle_list, next(scan_data_gen))
+#mcl.draw_map_state(wean_hall_map, new_particle_list, ax=ax)
+for _ in range(20):
+    new_particle_list = mcl.mcl_update(new_particle_list, next(scan_data_gen))
+#fig, ax = plt.subplots(figsize=(40,40))
+#mcl.draw_map_state(wean_hall_map, new_particle_list, ax=ax)
 
-class ParticleMap(object):
-    def __init__(self, ax, global_map, particle_list):
-        self.ax = ax
-        self.global_map = global_map
-        self.particle_list = particle_list
-        mcl.draw_map_state(global_map, particle_list[::50], ax=self.ax)
-        self.i = 1
+p = mcl.robot_particle(wean_hall_map, laser)
+p.new_pose_from_sample_error()
 
-    def update(self, message):
-        self.particle_list = mcl.mcl_update(self.particle_list, message, 
-                                            target_particles=300) # Update
-        if self.i % 20 == 0:# Plot every 10th message
-            plt.cla()        
-            mcl.draw_map_state(self.global_map, self.particle_list, self.ax)
-            #print(self.i, "  ", len(self.particle_list))
-            print(pd.Series([p.weight for p in particle_list]).describe())
-        self.i += 1
+new_particle_list = mcl.mcl_update(new_particle_list, next(scan_data_gen))
 
-fig, ax = plt.subplots()
-pmap = ParticleMap(ax, wean_hall_map, particle_list)
-
-print("5")
-#plt.show()
-# pass a generator in "emitter" to produce data for the update func
-ani = animation.FuncAnimation(fig, pmap.update, logdata_scans.values, interval=50,
-                              blit=False, repeat=False)
-print("6")
-plt.show()
+print(len([p.weight for p in particle_list]))
