0fd2b0e8eb8a099d722fa0b2c53016b07f505c38
markmellors <mark.mellors@gmail.com>
2020-02-05 21:52:28 +0000

    now adds map resolution to first line

--- png_to_csv.py
+++ png_to_csv.py
@@ -7,12 +7,13 @@ folder = "data/map/"
 file =  folder + map + ".png"
 image = ImageOps.grayscale(Image.open(file))
 img = np.array(image)/255
-
+resolution = [10]
 
 def csvWriter(fil_name, nparray):
   example = nparray.tolist()
   with open(folder+fil_name+'.dat', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerow(resolution)
     writer.writerows(example)
 
 csvWriter(map, img)
