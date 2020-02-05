be72b783711b53e6d150f648dd4870806043cbfc
markmellors <mark.mellors@gmail.com>
2020-02-05 13:54:42 +0000

    first draft at converting png map images into csv files

---
+++ png_to_csv.py
@@ -0,0 +1,18 @@
+from PIL import Image, ImageOps, ImageEnhance
+import numpy as np
+import os, os.path, time
+import csv
+map = "minesweeper"
+folder = "data/map/" 
+file =  folder + map + ".png"
+image = ImageOps.grayscale(Image.open(file))
+img = np.array(image)/255
+
+
+def csvWriter(fil_name, nparray):
+  example = nparray.tolist()
+  with open(folder+fil_name+'.dat', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=' ')
+    writer.writerows(example)
+
+csvWriter(map, img)
